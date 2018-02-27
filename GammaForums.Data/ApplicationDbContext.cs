fcfb832cf0b972fdecb4228938d3d96b0b8329cd
Brian Wright <bwright@webstaurantstore.com>
2018-02-27 14:46:40 -0500

    Initial commit, create data project

---
+++ GammaForums.Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using GammaForums.Data.Models;
+
+namespace GammaForums.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies { get; set; }
+
+    }
+}
