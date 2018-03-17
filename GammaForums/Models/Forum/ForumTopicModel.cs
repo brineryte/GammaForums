d4be34dd0e10a5d0aa465af441197c1da2dd90b4
Brian Wright <bwright@webstaurantstore.com>
2018-03-17 15:47:20 -0400

    Added styles, Formatted Topic, PostIndex

--- GammaForums/Models/Forum/ForumTopicModel.cs
+++ GammaForums/Models/Forum/ForumTopicModel.cs
@@ -8,5 +8,6 @@ namespace GammaForums.Models.Forum
     {
         public ForumListingModel Forum { get; set; }
         public IEnumerable<PostListingModel> Posts { get; set; } 
+        public string SearchQuery { get; set; }
     }
 }
